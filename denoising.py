5bc0aaa0e82cfdfc35fe270943392f885da574f7
rajatgoyal715 <rajatgoyal715@gmail.com>
2017-04-21 22:40:10 +0530

    Added noised images

--- denoising.py
+++ denoising.py
@@ -2,14 +2,14 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-file_name = 'noised/fest'
+file_name = 'fest2'
 file_extension = ".jpeg"
-img = cv2.imread(file_name + file_extension)
+file_input = file_name + file_extension
+img = cv2.imread(file_input)
 
 dst = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
 
-file_output = "denoised/" + file_name + "_denoised" + file_extension
-cv2.imwrite(file_output, dst)
+cv2.imwrite(file_name + "_denoised" + file_extension, dst)
 
 plt.subplot(121), plt.imshow(img)
 plt.subplot(122), plt.imshow(dst)
