65a71213c5994c7b399ce85b6e2e7eaa49402989
Rajat Goyal <rajatgoyal715@gmail.com>
2018-08-21 17:58:43 +0530

    Update denoising script

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # ImageDenoising
 Denoising the noised image using OpenCV in Python.
 
-![Noised-Denoised](https://raw.githubusercontent.com/rajatgoyal715/ImageDenoising/master/Screenshots/denoising.png)
+![Fest Noised Denoised](https://raw.githubusercontent.com/rajatgoyal715/ImageDenoising/master/images/fest3_plt.jpeg)
+
+![Painting Noised Denoised](https://raw.githubusercontent.com/rajatgoyal715/ImageDenoising/master/images/painting_plt.jpg)
--- denoising.py
+++ denoising.py
@@ -1,16 +1,35 @@
-import numpy as np
 import cv2
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-file_name = 'fest'
-file_extension = ".jpeg"
-file_input = file_name + file_extension
-img = cv2.imread(file_input)
+file_path = input("Enter the image path\n")
+slash_index = file_path.rfind('/')
 
-dst = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
+if slash_index == -1:
+    file_full_name = file_path
+else:
+    file_extra_path = file_path[:slash_index]
+    file_full_name = file_path[slash_index+1:]
 
-cv2.imwrite(file_name + "_denoised" + file_extension, dst)
+dot_index = file_full_name.rfind('.')
+file_name = file_full_name[:dot_index]
+file_extension = file_full_name[dot_index+1:]
 
-plt.subplot(121), plt.imshow(img)
-plt.subplot(122), plt.imshow(dst)
+input_img = mpimg.imread(file_path)
+
+output_img = cv2.fastNlMeansDenoisingColored(input_img, None, 10, 10, 7, 21)
+
+output_file_name = file_name + "_denoised"
+output_file_full_name = output_file_name + '.' + file_extension
+output_file_path = output_file_full_name if slash_index == -1 else file_extra_path + '/' + output_file_full_name
+
+plt_file_name = file_name + "_plt"
+plt_file_full_name = plt_file_name + '.' + file_extension
+plt_file_path = plt_file_full_name if slash_index == -1 else file_extra_path + '/' + plt_file_full_name
+
+mpimg.imsave(output_file_path, output_img)
+
+plt.subplot(121), plt.imshow(input_img)
+plt.subplot(122), plt.imshow(output_img)
+plt.savefig(plt_file_path)
 plt.show()
